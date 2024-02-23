86fc8ba47960fff926f36b374b50285a037f4566
Talles <talves2503@gmail.com>
2024-02-22 21:46:37 -0300

    static mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <header>
-            
+        <header className="flex bg-blue-dark px-5 py-3 justify-between">
+            <img src="/img/header/logo-coelhoBD.svg" alt="" />
+
+            <button>
+                <img className="hover:scale-110 transition-all" src="/img/header/hamburger-menu.svg" alt="" />
+            </button>
         </header>
     )
 };
