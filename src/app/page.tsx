f4b890cd4de5983a893d35d9ada5dd4352c1e0fa
Eduardo Siles <eduardosiles2003@gmail.com>
2024-02-23 23:18:01 -0300

    Mobile hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,67 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      {/* hero section */}
+      <section className="flex items-center justify-center bg-hero-pattern bg-cover bg-no-repeat h-[700px] max-w-full text-white mx-auto py-16">
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col items-start gap-9 z-10 p-9 absolute md:static">
+            <h1 className="text-blue-light font-edo text-5xl w-full">
+              Bateria Danada Unifei
+            </h1>
+
+            <p className="font-sugo leading-5 max-w-96 text-lg">
+              A Bateria Danada Unifei carrega no peito o nome da Universidade
+              Federal de Itajubá.
+            </p>
+            <p className="font-sugo leading-5 max-w-96 text-lg">
+              Há décadas tem encantado e animado a comunidade universitária,
+              participando de eventos, torneios, shows e serviços para a
+              comunidade de Itajubá. Representando o Sul de Minas Gerais
+              juntamente com o coelhão, segura que a Danada vem aí!
+            </p>
+
+            <picture>
+              <img
+                className="-my-10 md:hidden"
+                src="/img/hero/uh-que-azar.png"
+                alt="Mancha de tinta roxa com o escrito UUHHH QUE AZAR!! no meio"
+              />
+            </picture>
+
+            <a
+              className="flex items-center justify-center gap-3 p-5 w-52 bg-blue-dark font-sugo text-xl rounded-lg shadow-lg"
+              href="#contato"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <picture>
+                <img
+                  src="/img/hero/phone-icon.svg"
+                  alt="Ícone de um telefone branco"
+                />
+              </picture>
+              Entre em contato!
+            </a>
+          </div>
+
+          <div className="flex flex-col justify-center absolute md:static">
+            <picture>
+              <img
+                className="h-full opacity-60 overflow-hidden relative z-0"
+                src="/img/hero/coelhao.png"
+                alt=""
+              />
+              <picture>
+                <img
+                  className="hidden md:block"
+                  src="/img/hero/uh-que-azar.png"
+                  alt="Mancha de tinta roxa com o escrito UUHHH QUE AZAR!! no meio"
+                />
+              </picture>
+            </picture>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,37 +8,39 @@ const config: Config = {
   ],
   theme: {
     screens: {
-        'md': '480px',
-        'lg' : '1140px',
+      md: "480px",
+      lg: "1140px",
     },
     colors: {
-        purple: {
-            light: '#7429e7',
-            regular: '#4e1c9b'
-        },
-        blue: {
-            light: '#049fdc',
-            regular: '#166ba1',
-            dark: '#112c4a',
-        },
-        black: '#000000',
-        white: '#ffffff',
-        gray: '#d9d9d9'
+      purple: {
+        light: "#7429e7",
+        regular: "#4e1c9b",
+      },
+      blue: {
+        light: "#049fdc",
+        regular: "#166ba1",
+        dark: "#112c4a",
+      },
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#d9d9d9",
     },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "nav-hover" : "url('/img/header/bg-nav-hover.svg')"
+        "nav-hover": "url('/img/header/bg-nav-hover.svg')",
+        "hero-pattern": "url('/img/hero/bg-hero.png')",
       },
       fontFamily: {
-        'slowdex': ['Slowdex'],
-        'sugo': ['Sugo Pro']
+        slowdex: ["Slowdex"],
+        sugo: ["Sugo Pro"],
+        edo: ["Edo SZ"],
       },
       transitionProperty: {
-        'height': 'height',
-      }
+        height: "height",
+      },
     },
   },
   plugins: [],
